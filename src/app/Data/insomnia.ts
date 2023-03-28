fcdb0da64913d64906a4741fbb61c0f55866111a
Gisele <giginisimura@gmail.com>
2023-03-28 20:05:10 -0300

    ajustes

--- src/app/Data/insomnia.ts
+++ src/app/Data/insomnia.ts
@@ -1,7 +1,7 @@
 export const data = [
     {
         "name": "Login - realizando login e gerando Token",
-        "img": "assets/Image/insomnia/InsominiaLogin.png",
+        "img": "assets/Image/insomnia/insominiaLoginToken.png",
         "descriptionImg": "Endpoint Login",
         "conteudoItem": "EndPoint  realizando um Post(criando) um login, após realização da ação do mesmo, vemos na aba a direita da imagem o retorno do token gerado para a autorização do usuário.",
         "id": "1"
@@ -9,28 +9,28 @@ export const data = [
     {
         "name": "Get (Book)- Buscando os livros ",
         "img": "assets/Image/insomnia/InsominiaGet.png",
-        "descriptionImg": "Get_User",
+        "descriptionImg": "Endpoint_Get_Book",
         "conteudoItem": "Get (Book) -  Ação que retorna como requisição todos os livros cadastrados.",
         "id": "2"
     },
     {
         "name": "Post (Book) - Cadastrando Livro ",
         "img": "assets/Image/insomnia/InsominiaCreate.png",
-        "descriptionImg": "post_user",
+        "descriptionImg": "Endpoint_Post_Book",
         "conteudoItem": " Post ( Book) - Criando um novo cadastro na entidade livro, com sua propriedades.",
         "id": "3"
     },
     {
-        "name": "Get By Id",
-        "img": "assets/Image/insomnia/InsominiaGet.png",
-        "descriptionImg": "",
-        "conteudoItem": "",
+        "name": "Get By Id (Book)",
+        "img": "assets/Image/insomnia/getById.png",
+        "descriptionImg": "Endpoint_getById_Book",
+        "conteudoItem": "Busca o livro pela sua chave de identificação de cadastro.",
         "id": "4",
     },
     {
         "name": "Put (Book) - atualizando livro ",
         "img": "assets/Image/insomnia/insominiaUpdate.png",
-        "descriptionImg": "Put_Book",
+        "descriptionImg": "Endpoint_Put_Book",
         "conteudoItem": "Put(Book) - Nesse endpoint podemos fazer a atualização dos dados de um livro cadastrado, buscando o livro pela sua chave de autenticação, e atualizando para os dados redefinidos.",
         "id": "5"
     }
